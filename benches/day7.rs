70fb1a846afea5b3c9c0705557d36b25fb9fd219
Marcel Witoschek <92092148+Marciland@users.noreply.github.com>
2025-11-28 12:13:52 +0100

    Initial commit

---
+++ benches/day7.rs
@@ -0,0 +1,20 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+
+const fn part1() {
+    //
+}
+
+const fn part2() {
+    //
+}
+
+fn bench_part1(c: &mut Criterion) {
+    c.bench_function("day7 part1", |b| b.iter(part1));
+}
+
+fn bench_part2(c: &mut Criterion) {
+    c.bench_function("day7 part2", |b| b.iter(part2));
+}
+
+criterion_group!(benches, bench_part1, bench_part2);
+criterion_main!(benches);
