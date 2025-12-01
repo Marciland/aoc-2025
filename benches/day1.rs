51aedc4beae74ddaae5a429dd2bd6094f090ffa4
Marcel Witoschek <92092148+Marciland@users.noreply.github.com>
2025-12-01 13:54:28 +0100

    day 1 (#1)
    * star 1 done
    
    * star 2 done

--- benches/day1.rs
+++ benches/day1.rs
@@ -1,19 +1,89 @@
+use aoc_2025::read_input;
 use criterion::{Criterion, criterion_group, criterion_main};
 
-const fn part1() {
-    //
+const START: i16 = 50;
+
+fn part1(input: &Vec<String>) -> u16 {
+    let mut current = START;
+    let mut result = 0;
+
+    for l in input {
+        let (direction, distance_str) = l.split_at(1);
+        let distance = distance_str.parse::<i16>().unwrap();
+
+        match direction {
+            "L" => {
+                current -= distance;
+            }
+            "R" => {
+                current += distance;
+            }
+            _ => panic!(),
+        }
+
+        current %= 100;
+
+        if current == 0 {
+            result += 1;
+        }
+    }
+
+    result
 }
 
-const fn part2() {
-    //
+fn part2(input: &Vec<String>) -> u16 {
+    let mut current = START;
+    let mut result = 0;
+
+    for l in input {
+        let (direction, distance_str) = l.split_at(1);
+        let distance = distance_str.parse::<i16>().unwrap();
+
+        match direction {
+            "L" => {
+                if current.is_positive() && (current - distance).is_negative() {
+                    result += 1;
+                }
+                current -= distance;
+            }
+            "R" => {
+                if current.is_negative() && (current + distance).is_positive() {
+                    result += 1;
+                }
+                current += distance;
+            }
+            _ => panic!(),
+        }
+
+        let stepped_over = current.unsigned_abs() / 100;
+        current %= 100;
+
+        if current == 0 {
+            result += 1;
+            result += stepped_over.saturating_sub(1);
+        } else {
+            result += stepped_over;
+        }
+    }
+    result
 }
 
 fn bench_part1(c: &mut Criterion) {
-    c.bench_function("day1 part1", |b| b.iter(part1));
+    let input = read_input(1);
+
+    let result = part1(&input);
+    println!("day1 part1 result: {result}");
+
+    c.bench_function("day1 part1", |b| b.iter(|| part1(&input)));
 }
 
 fn bench_part2(c: &mut Criterion) {
-    c.bench_function("day1 part2", |b| b.iter(part2));
+    let input = read_input(1);
+
+    let result = part2(&input);
+    println!("day1 part2 result: {result}");
+
+    c.bench_function("day1 part2", |b| b.iter(|| part2(&input)));
 }
 
 criterion_group!(benches, bench_part1, bench_part2);
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
 
+#[must_use]
+#[allow(clippy::missing_panics_doc)]
+pub fn read_input(day: u8) -> Vec<String> {
+    let file_path = PathBuf::from(format!("input/{day}.input"));
+    let file = File::open(file_path).unwrap_or_else(|_| panic!("missing file {day}"));
+    let reader = BufReader::new(file);
+
+    reader
+        .lines()
+        .map(|line| line.expect("Invalid input formatting"))
+        .collect()
+}
