bbdb32abec9630934cd0adb563966c4919405462
Marcel Witoschek <marcel.witoschek@gdata.de>
2025-12-01 13:44:43 +0100

    star 2 done

--- benches/day1.rs
+++ benches/day1.rs
@@ -31,8 +31,41 @@ fn part1(input: &Vec<String>) -> u16 {
     result
 }
 
-const fn part2(_input: &Vec<String>) {
-    //
+fn part2(input: &Vec<String>) -> u16 {
+    let mut current = START;
+    let mut result = 0;
+
+    for l in input {
+        let (direction, distance_str) = l.split_at(1);
+        let distance = distance_str.parse::<i16>().unwrap();
+
+        match direction {
+            "L" => {
+                if current.is_positive() && (current - distance).is_negative() {
+                    result += 1;
+                }
+                current -= distance;
+            }
+            "R" => {
+                if current.is_negative() && (current + distance).is_positive() {
+                    result += 1;
+                }
+                current += distance;
+            }
+            _ => panic!(),
+        }
+
+        let stepped_over = current.unsigned_abs() / 100;
+        current %= 100;
+
+        if current == 0 {
+            result += 1;
+            result += stepped_over.saturating_sub(1);
+        } else {
+            result += stepped_over;
+        }
+    }
+    result
 }
 
 fn bench_part1(c: &mut Criterion) {
@@ -47,8 +80,8 @@ fn bench_part1(c: &mut Criterion) {
 fn bench_part2(c: &mut Criterion) {
     let input = read_input(1);
 
-    // let result = part2(&input);
-    // println!("day1 part2 result: {result}");
+    let result = part2(&input);
+    println!("day1 part2 result: {result}");
 
     c.bench_function("day1 part2", |b| b.iter(|| part2(&input)));
 }
