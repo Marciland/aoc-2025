372a57ef5db82683b31c7c8f3f9f5595cdc553fa
Marcel Witoschek <marcel.witoschek@gdata.de>
2025-12-05 01:19:01 +0100

    star 1 done

--- benches/day4.rs
+++ benches/day4.rs
@@ -1,8 +1,94 @@
 use aoc_2025::fetch_aoc_input;
 use criterion::{Criterion, criterion_group, criterion_main};
+use std::collections::HashMap;
 
-const fn part1(_input: &str) {
-    //
+#[derive(Eq, PartialEq, Hash)]
+struct Pos2D {
+    x: i32,
+    y: i32,
+}
+
+impl Pos2D {
+    fn neighbours(&self) -> Vec<Self> {
+        vec![
+            Self {
+                x: self.x - 1,
+                y: self.y - 1,
+            },
+            Self {
+                x: self.x,
+                y: self.y - 1,
+            },
+            Self {
+                x: self.x + 1,
+                y: self.y - 1,
+            },
+            Self {
+                x: self.x - 1,
+                y: self.y,
+            },
+            Self {
+                x: self.x + 1,
+                y: self.y,
+            },
+            Self {
+                x: self.x - 1,
+                y: self.y + 1,
+            },
+            Self {
+                x: self.x,
+                y: self.y + 1,
+            },
+            Self {
+                x: self.x + 1,
+                y: self.y + 1,
+            },
+        ]
+    }
+}
+
+fn paper_neighbours(grid: &HashMap<Pos2D, bool>, pos: &Pos2D) -> usize {
+    pos.neighbours()
+        .iter()
+        .filter(|p| grid.contains_key(p) && *grid.get(p).unwrap())
+        .count()
+}
+
+fn parse_grid(input: &str) -> HashMap<Pos2D, bool> {
+    let lines: Vec<String> = input.split('\n').map(str::to_string).collect();
+    let mut grid = HashMap::with_capacity(input.len());
+
+    for (y, line) in lines.iter().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            grid.insert(
+                Pos2D {
+                    x: x.try_into().unwrap(),
+                    y: y.try_into().unwrap(),
+                },
+                c == '@',
+            );
+        }
+    }
+
+    grid
+}
+
+fn part1(input: &str) -> u64 {
+    let grid = parse_grid(input);
+
+    let mut result = 0;
+
+    for (pos, is_paper) in &grid {
+        if !is_paper {
+            continue;
+        }
+
+        if paper_neighbours(&grid, pos) < 4 {
+            result += 1;
+        }
+    }
+
+    result
 }
 
 const fn part2(_input: &str) {
@@ -10,10 +96,10 @@ const fn part2(_input: &str) {
 }
 
 fn bench_part1(c: &mut Criterion) {
-    let input = fetch_aoc_input(1);
+    let input = fetch_aoc_input(4);
 
-    // let result = part1(&input);
-    // println!("day4 part1 result: {result}");
+    let result = part1(&input);
+    println!("day4 part1 result: {result}");
 
     c.bench_function("day4 part1", |b| b.iter(|| part1(&input)));
 }
