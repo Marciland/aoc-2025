49216cafbd824c679a95371883298a3e4ed95fc4
Marcel Witoschek <marcel.witoschek@gdata.de>
2025-12-05 11:42:53 +0100

    star 1 done

--- benches/day5.rs
+++ benches/day5.rs
@@ -1,8 +1,51 @@
 use aoc_2025::fetch_aoc_input;
 use criterion::{Criterion, criterion_group, criterion_main};
+use std::ops::RangeInclusive;
 
-const fn part1(_input: &str) {
-    //
+struct Database {
+    ranges: Vec<RangeInclusive<u64>>,
+    ids: Vec<u64>,
+}
+
+fn parse_input(input: &str) -> Database {
+    let mut ranges = Vec::new();
+    let mut ids = Vec::new();
+
+    let mut reached_middle = false;
+    for line in input.split('\n') {
+        if line.is_empty() {
+            reached_middle = true;
+            continue;
+        }
+        if reached_middle {
+            ids.push(line.parse().unwrap());
+            continue;
+        }
+        let (start, end): (u64, u64) = line
+            .split_once('-')
+            .map(|(l, r)| (l.parse().unwrap(), r.parse().unwrap()))
+            .unwrap();
+        ranges.push(start..=end);
+    }
+
+    Database { ranges, ids }
+}
+
+fn part1(input: &str) -> u64 {
+    let mut result = 0;
+
+    let database = parse_input(input);
+
+    for id in database.ids {
+        for range in &database.ranges {
+            if range.contains(&id) {
+                result += 1;
+                break;
+            }
+        }
+    }
+
+    result
 }
 
 const fn part2(_input: &str) {
@@ -10,17 +53,17 @@ const fn part2(_input: &str) {
 }
 
 fn bench_part1(c: &mut Criterion) {
-    let input = fetch_aoc_input(1);
+    let input = fetch_aoc_input(5);
 
-    // let result = part1(&input);
-    // println!("day5 part1 result: {result}");
+    let result = part1(&input);
+    println!("day5 part1 result: {result}");
 
     c.bench_function("day5 part1", |b| b.iter(|| part1(&input)));
 }
 
 fn bench_part2(c: &mut Criterion) {
-    let input = fetch_aoc_input(1);
-
+    // let input = fetch_aoc_input(5);
+    let input = include_str!("5.txt").to_string();
     // let result = part2(&input);
     // println!("day5 part2 result: {result}");
 
